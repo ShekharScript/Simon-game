b8036bbef5059aee47e9cc1e40a3c6893fd0d5d5
ShekharScript <shekhar5980official@gmail.com>
2025-07-11 14:41:19 +0530

    Update and rename index.js to game.js

--- game.js
+++ game.js
@@ -4,17 +4,17 @@ let userClickedPattern = [];
 let gamePattern = [];
 buttonColors = ["red", "blue", "green", "yellow"]
 let level_value = 0
-keypress_count = 0
+let keypress_count = 0
 
 // show increased level :1,2,3
-function show_changed_level(level_value) {
+function show_changed_level() {
     $("#level-title").text("Level " + level_value)
 }
 
 //generating random sequence
 function nextSequence() {
     level_value = level_value + 1
-    show_changed_level(level_value)
+    show_changed_level()
     return random_no = Math.floor(Math.random() * 4)
 }
 
@@ -24,18 +24,16 @@ function calling_next_sequence() {
     randomChoosenColor = buttonColors[nextSequence()];
     animateFlash(randomChoosenColor)
     gamePattern.push(randomChoosenColor)
-    console.log("gamerPattern :" + gamePattern+">>>>>")
+    console.log("gamerPattern :" + gamePattern + ">>>>>")
 }
+
 
 
 // what to do when key is pressed
 document.addEventListener("keypress", function () {
     keypress_count += 1
-    //storing the random color sequence and calling nextSequence() for first keypress
-    if (keypress_count === 1) {
-        calling_next_sequence()
-    }
-    if(hasUserFalied === true){
+    //calling nextSequence() indirectly for first keypress and when user has failed
+    if (hasUserFalied === true || keypress_count ===1 ) {
         startOver();
     }
 })
@@ -43,20 +41,20 @@ document.addEventListener("keypress", function () {
 
 
 // startOver function resets all values
-function startOver(){
+function startOver() {
     gamePattern = []
     userClickedPattern = []
     level_value = 0
-    hasUserFalied = false ;
+    hasUserFalied = false;
     calling_next_sequence()
-    i=0
+    i = 0
 }
 
 
 
 //play sound and flash animation
 function animateFlash(color) {
-    color_item = $("#" + color)
+    let color_item = $("#" + color)
     color_item.addClass("flash")
     playSound(color);
     setTimeout(() => {
@@ -69,7 +67,7 @@ function animateFlash(color) {
 
 //now play sound for the selected color
 function playSound(ele) {
-    console.log("<<<<<<<this is a sound check !!")
+    // console.log("<<<<<<<this is a sound check !!")
     switch (ele) {
         case "red":
             var red = new Audio('sounds/red.mp3')
@@ -101,7 +99,7 @@ function playSound(ele) {
 
 // animation on btn click 
 function animateClick(currentColor) {
-    color = $("#" + currentColor)
+    let color = $("#" + currentColor)
     color.addClass("pressed")
     setTimeout(() => {
         color.removeClass("pressed")
@@ -123,53 +121,63 @@ function animateClick(currentColor) {
 hasUserFalied = false
 i = 0
 $(".btn").on("click", function () {
-    if (i < gamePattern.length) { 
+    //when user clicks button before a keypress
+    if (keypress_count == 0) {
+        gameOver();
+        return
+    }
+    
+    if (i < gamePattern.length) {
         // there is no need for :  "if (i < gamePattern.length)" , it can work without this too
         var userChosenColour = this.id
         playSound(this.id)
         animateClick(this.id)
         userClickedPattern.push(userChosenColour)
         console.log("userClickedPattern : " + userClickedPattern);
-            if(IsArraysEqual(gamePattern , userClickedPattern , i+1)){
-                i = i + 1;
-                console.log("i = " + i)
-            }
-            else {
-                hasUserFalied = true
-                var wrong_answer = new Audio('sounds/wrong.mp3')
-                wrong_answer.play()
-                $("body").addClass("game-over")
-                setTimeout(() => {
-                    $("body").removeClass("game-over")
-                }, 200)
-                $("h1").text("Game Over, Press Any Key to Restart")
-            }
+        if (IsArraysEqual(gamePattern, userClickedPattern, i + 1)) {
+            i = i + 1;
+            console.log("i = " + i)
+        }
+        else {
+            gameOver();
+        }
     }
 
-    if (i === gamePattern.length){
-            // console.log("arrays are equal")
-            setTimeout(() => {
+    if (i === gamePattern.length) {
+        console.log("arrays are equal")
+        setTimeout(() => {
             calling_next_sequence()
-            }, 1000);
-            //reset the user clicked parameters
-            userClickedPattern = [];
-            i=0;
-            // console.log(i)
-        }
-        
+        }, 1000);
+        //reset the user clicked parameters
+        userClickedPattern = [];
+        i = 0;
+        console.log(i)
     }
+
+}
 )
 
+function gameOver() {
+    hasUserFalied = true
+    var wrong_answer = new Audio('sounds/wrong.mp3')
+    wrong_answer.play()
+    $("body").addClass("game-over")
+    setTimeout(() => {
+        $("body").removeClass("game-over")
+    }, 200)
+    $("h1").text("Game Over, Press Any Key to Restart")
+}
 
 
-function IsArraysEqual(a, b , P) {
-    let flag = 1 
-    for(j =0 ; j < P ; j++){
+
+function IsArraysEqual(a, b, P) {
+    let flag = 1
+    for (j = 0; j < P; j++) {
         if (gamePattern[j] !== userClickedPattern[j])
-            flag = 0 
-        }
-    if(flag==0) return false ;
+            flag = 0
+    }
+    if (flag == 0) return false;
     else return true;
-        
+
 }
 
